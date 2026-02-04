e27bf36d6fb805d8fea4ad54dbd90dabfa4d243c
LazyKangeroo <fvantonder00@gmail.com>
2026-02-04 21:49:17 +0200

    Improved deposit system
    Can now choose the type of deposits (frequency).

--- main.py
+++ main.py
@@ -1,20 +1,4 @@
-def calAnn(timeperiod, amnt, deposits, rate):
-    new_amnt = amnt
-    while timeperiod > 0:
-        new_amnt += deposits
-        new_amnt += new_amnt * rate / 100 / 12
-        timeperiod -= 1
-    print(f"\n= R{round(new_amnt,2)}")
-
-
-def calMonthly(timeperiod, amnt, deposits, rate):
-    new_amnt = amnt
-    while timeperiod > 0:
-        new_amnt += deposits
-        new_amnt += new_amnt * rate / 100
-        timeperiod -= 1
-    print(f"\n= R{round(new_amnt,2)}")
-
+#* Welcome message
 print('---Welcome to the Investment Calculator---\n')
 
 start_amnt = float(input("Enter starting amount: \n > R"))
@@ -27,12 +11,51 @@ rate = float(input("\nEnter rate in %:\n >"))
 print("\n//NOTE// Period of time is monthly.\n")
 timeperiod = int(input("Enter period of time:\n >> "))
 
-deposits = float(input("\nMontly Deposits into Account:\n > R"))
+#? Deposits
+if input('Do you deposit any additional amounts into this account?\n (y/n) >> ')[0].upper() == 'Y':
+    deposits = float(input("\nDeposits into Account:\n > R"))
 
-match rate_type:
-    case 1:
-        calAnn(timeperiod,start_amnt,deposits,rate)
-    case 2:
-        calMonthly(timeperiod,start_amnt,deposits,rate)
-    case _:
-        print("\n[!] Error: Rate Type incorrect input")
+    deposit_options = int(input("When do you make deposits?\n (1) Montly\n (2) Every second month\n (3) Quarterly \n (4) Semi-Annually\n >"))
+    match deposit_options:
+        case 1:
+            frequency = 1
+        case 2:
+            frequency = 2
+        case 3:
+            frequency = 3
+        case 4:
+            frequency = 6
+        case _:
+            print("\n[!] Error: Deposit Options incorrect input")
+            exit()
+else:
+    deposits = 0
+    frequency = 0
+
+#! cal interest
+def ann(amnt,rate):
+    return amnt * rate / 100 / 12
+
+def monthly(amnt,rate):
+    return amnt * rate / 100
+
+new_amnt = start_amnt
+count_month = 1
+
+while timeperiod > 0:
+    if count_month == frequency:
+        new_amnt += deposits
+
+    match rate_type:
+        case 1:
+            new_amnt += ann(new_amnt,rate)
+        case 2:
+            new_amnt += monthly(new_amnt,rate)
+        case _:
+            print("\n[!] Error: Rate Type incorrect input")
+            exit()
+
+    timeperiod -= 1
+    count_month += 1
+
+print(f"\n= R{round(new_amnt,2)}")
